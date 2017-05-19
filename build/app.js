44c3da1f99f580ac68a802f112deeabc06085e77
Brian Chu <brianchu@D4331.NET>
2017-05-19 17:44:29 +0800

    init

--- build/app.js
+++ build/app.js
@@ -1,20 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-Date.prototype.yyyyMMddHHmmss = function () {
-    var date = this;
-    var year = date.getFullYear();
-    var month = date.getMonth() + 1;
-    var day = date.getDate();
-    var hh = date.getHours();
-    var mm = date.getMinutes();
-    var ss = date.getSeconds();
-    return "" + year +
-        (month < 10 ? "0" + month : month) +
-        (day < 10 ? "0" + day : day) +
-        (hh < 10 ? "0" + hh : hh) +
-        (mm < 10 ? "0" + mm : mm) +
-        (ss < 10 ? "0" + ss : ss);
-};
+require("./extend");
 var mqtt = require("mqtt");
 var client = mqtt.connect("wss://mbltest01.mqtt.iot.gz.baidubce.com:8884/mqtt", {
     username: "mbltest01/body",
@@ -40,13 +26,13 @@ client.on("connect", function (connack) {
         //console.log(JSON.stringify(err));
         //console.log(JSON.stringify(granted));
         if ((typeof err === "undefined" || err === null) && granted.some(function (value) { return value.topic === "body/#" && value.qos !== 128; })) {
-            var data = {
+            var iAm = {
                 who: "body",
                 whoAmI: "body",
                 when: new Date().yyyyMMddHHmmss()
             };
-            console.log("toEight/i.am" + " => " + JSON.stringify(data));
-            client.publish("toEight/i.am", JSON.stringify(data), function (err) {
+            console.log("toEight/i.am" + " => " + JSON.stringify(iAm));
+            client.publish("toEight/i.am", JSON.stringify(iAm), function (err) {
                 //console.log("publish");
                 //console.log(JSON.stringify(err));
             });
