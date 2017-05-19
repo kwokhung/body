2216a1176269a30c66ed9025638aa73832e59f88
Brian Chu <brianchu@D4331.NET>
2017-05-19 11:11:07 +0800

    init

--- build/app.js
+++ build/app.js
@@ -21,33 +21,34 @@ var client = mqtt.connect("wss://mbltest01.mqtt.iot.gz.baidubce.com:8884/mqtt", 
     password: "grSMaiLF8hiOtbPJFgXZdadTDBBKAY6I1KSNIKr+MgI="
 });
 client.on("connect", function (connack) {
-    console.log("on connect");
-    console.log(JSON.stringify(connack));
+    //console.log("on connect");
+    //console.log(JSON.stringify(connack));
     client.on("message", function (topic, message, packet) {
-        console.log("on message");
-        console.log(JSON.stringify(topic));
-        console.log(JSON.stringify(message));
-        console.log(JSON.stringify(packet));
+        //console.log("on message");
+        //console.log(JSON.stringify(topic));
+        //console.log(JSON.stringify(message));
+        //console.log(JSON.stringify(packet));
         console.log(topic + ": " + message.toString());
     });
     client.subscribe("fromEight/#", function (err, granted) {
-        console.log("subscribe");
-        console.log(JSON.stringify(err));
-        console.log(JSON.stringify(granted));
+        //console.log("subscribe");
+        //console.log(JSON.stringify(err));
+        //console.log(JSON.stringify(granted));
     });
     client.subscribe("body/#", function (err, granted) {
-        console.log("subscribe");
-        console.log(JSON.stringify(err));
-        console.log(JSON.stringify(granted));
+        //console.log("subscribe");
+        //console.log(JSON.stringify(err));
+        //console.log(JSON.stringify(granted));
         if ((typeof err === "undefined" || err === null) && granted.some(function (value) { return value.topic === "body/#" && value.qos !== 128; })) {
             var data = {
                 who: "body",
                 whoAmI: "body",
                 when: new Date().yyyyMMddHHmmss()
             };
+            console.log("toEight/i.am" + ": " + JSON.stringify(data));
             client.publish("toEight/i.am", JSON.stringify(data), function (err) {
-                console.log("publish");
-                console.log(JSON.stringify(err));
+                //console.log("publish");
+                //console.log(JSON.stringify(err));
             });
         }
     });
--- src/app.ts
+++ src/app.ts
@@ -29,28 +29,28 @@ let client = mqtt.connect("wss://mbltest01.mqtt.iot.gz.baidubce.com:8884/mqtt", 
 });
 
 client.on("connect", (connack) => {
-    console.log("on connect");
-    console.log(JSON.stringify(connack));
+    //console.log("on connect");
+    //console.log(JSON.stringify(connack));
 
     client.on("message", (topic, message, packet) => {
-        console.log("on message");
-        console.log(JSON.stringify(topic));
-        console.log(JSON.stringify(message));
-        console.log(JSON.stringify(packet));
+        //console.log("on message");
+        //console.log(JSON.stringify(topic));
+        //console.log(JSON.stringify(message));
+        //console.log(JSON.stringify(packet));
 
         console.log(topic + ": " + message.toString());
     });
 
     client.subscribe("fromEight/#", (err, granted) => {
-        console.log("subscribe");
-        console.log(JSON.stringify(err));
-        console.log(JSON.stringify(granted));
+        //console.log("subscribe");
+        //console.log(JSON.stringify(err));
+        //console.log(JSON.stringify(granted));
     });
 
     client.subscribe("body/#", (err, granted) => {
-        console.log("subscribe");
-        console.log(JSON.stringify(err));
-        console.log(JSON.stringify(granted));
+        //console.log("subscribe");
+        //console.log(JSON.stringify(err));
+        //console.log(JSON.stringify(granted));
 
         if ((typeof err === "undefined" || err === null) && granted.some(value => value.topic === "body/#" && value.qos !== 128)) {
             let data: any = {
@@ -59,9 +59,11 @@ client.on("connect", (connack) => {
                 when: new Date().yyyyMMddHHmmss()
             };
 
+            console.log("toEight/i.am" + ": " + JSON.stringify(data));
+
             client.publish("toEight/i.am", JSON.stringify(data), (err) => {
-                console.log("publish");
-                console.log(JSON.stringify(err));
+                //console.log("publish");
+                //console.log(JSON.stringify(err));
             });
         }
     });
